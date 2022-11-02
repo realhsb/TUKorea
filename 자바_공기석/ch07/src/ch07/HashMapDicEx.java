54218c045ca03d5b9c731b779c6952165467ca83
한수빈 <realhsb@gmail.com>
2022-11-03 00:35:35 +0900

    ch07

---
+++ 자바_공기석/ch07/src/ch07/HashMapDicEx.java
@@ -0,0 +1,31 @@
+package ch07;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class HashMapDicEx {
+	public static void main(String[] args) {
+		HashMap<String, String> dic = new HashMap<String, String>();
+		
+		dic.put("baby", "아기");
+		dic.put("love", "사랑");
+		dic.put("apple", "사과");
+		
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			System.out.println("찾고 싶은 단어 : ");
+			String eng = scanner.next();
+			if(eng.equals("exit")) {
+				System.out.println("종료");
+				break;
+			}
+			String kor = dic.get(eng);
+			if(kor == null) 
+				System.out.println(eng + "는 없는 단어");
+			else 
+				System.out.println(kor);
+		}
+		scanner.close();
+	}
+
+}
---
+++ 자바_공기석/ch07/src/ch07/HashMapScoreEx.java
@@ -0,0 +1,7 @@
+package ch07;
+
+public class HashMapScoreEx {
+	public static void main(String[] args) {
+		
+	}
+}
